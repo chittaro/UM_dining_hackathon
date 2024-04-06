ce053726eb8b5f7f903d0bf3ce3628a4dcbbf096
HIMARITAMURA <HIMARI@UMICH.EDU>
2024-04-06 13:27:58 -0400

    changes in main

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
- 
+from flask import Flask, jsonify
+import pandas as pd
+
+from scraping.data import scrape_and_send
 
 app = Flask(__name__)
 
 @app.route("/home")
 def home():
     return "hi"
+
+def handle_scraping():
+    data = scrape_and_send()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
